d843460262b4375693554ff7bf60ca45e63a8c25
DeathWalker47 <fizer_00@mail.ru>
2021-07-02 23:19:06 +0300

    added about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
   
+  $('.slider').slick({
+    infinite: true,
+    arrows: true,
+    speed: 1200,
+    slidesToShow: 1,
+  });
+
+  $('.about__portfolio-link').hover(function(){
+    $('.about__portfolio-images').toggleClass('about__portfolio-images--active');
+  });
+
 });
